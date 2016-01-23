c47800e9aaf810a61e01d34049b143b3e0ad3ad2
Francois Dang Ngoc <francois.dangngoc@gmail.com>
2016-01-23 18:35:50 -0500

    simplified day 3

--- adventofcode/day03.py
+++ adventofcode/day03.py
@@ -8,38 +8,34 @@ import util
 class Day3(object):
     def __init__(self, input_file):
         self._input_file = input_file
+        self._moves = self._get_moves()
 
     def _get_moves(self):
+        def get_move(c):
+            if c == '>':
+                return (1, 0)
+            elif c == '<':
+                return (-1, 0)
+            elif c == '^':
+                return (0, -1)
+            elif c == 'v':
+                return (0, 1)
+
         with open(self._input_file) as fd:
-            content = [c for c in fd.read() if c in '><^v']
-            for c in content:
-                if c == '>':
-                    yield (1, 0)
-                elif c == '<':
-                    yield (-1, 0)
-                elif c == '^':
-                    yield (0, -1)
-                elif c == 'v':
-                    yield (0, 1)
+            return [get_move(c) for c in fd.read() if c in '><^v']
 
     @staticmethod
     def _add_coords((x1, y1), (x2, y2)):
         return x1 + x2, y1 + y2
 
     def part1(self):
-        all_coords = util.accumulate(self._get_moves(), self._add_coords)
+        all_coords = util.accumulate(self._moves, self._add_coords)
         return len(set(all_coords))
 
     def part2(self):
-        all_moves = list(self._get_moves())
         # even moves for santa and odd moves for robo
-        santa_moves, robo_moves = [
-            (move for _, move in moves) for moves in util.partition(
-                    lambda (x, y): x % 2 == 0,
-                    enumerate(all_moves)
-            )
-            ]
-
+        santa_moves = self._moves[::2]
+        robo_moves = self._moves[1::2]
         santa_coords = util.accumulate(santa_moves, self._add_coords)
         robo_coords = util.accumulate(robo_moves, self._add_coords)
         return len(set(itertools.chain(santa_coords, robo_coords)))
